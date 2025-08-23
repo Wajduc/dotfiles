9fc84e0fa35e6f30871dffb14a8d7b181aa42be7
Wajduc <miha.rijavec24@gmail.com>
2025-08-23 16:44:04 +0200

    changed some stuff

--- arch_setup.sh
+++ arch_setup.sh
@@ -81,14 +81,7 @@ else
   exit
 fi
 
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-
 cd ~/dotfiles || exit
-rm ~/.zshrc
-stow -vt ~ zsh
-stow -vt ~ p10k
 stow -vt ~ alacritty
 stow -vt ~ hypr
 stow -vt ~ waybar
