4bbdca9ab91d4677ac1b52765c4eae1e36012b1e
Wajduc <miha.rijavec24@gmail.com>
2025-08-23 16:22:03 +0200

    changed some stuff

---
+++ post_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+rm ~/.zshrc
+stow -vt ~ zsh
+stow -vt ~ p10k
